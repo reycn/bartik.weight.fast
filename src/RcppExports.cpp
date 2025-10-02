3cbf2986f402b17b761a049ad2c474db53deca42
Rongxin <rongxin@u.nus.edu>
2025-10-02 21:40:19 +0800

    fix: data type cache

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,6 +6,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // ComputeAlphaBeta
 List ComputeAlphaBeta(arma::vec y, arma::vec x, arma::mat WW, arma::vec weight, arma::mat Z, arma::vec G);
 RcppExport SEXP _bartik_weight_ComputeAlphaBeta(SEXP ySEXP, SEXP xSEXP, SEXP WWSEXP, SEXP weightSEXP, SEXP ZSEXP, SEXP GSEXP) {
